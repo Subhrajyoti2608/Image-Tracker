fa86d40898a3cb6d2ae0c470d715e1fb73a3bbe5
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-02-11 15:27:39 +0530

    Add files via upload

---
+++ play-on-click.js
@@ -0,0 +1,29 @@
+AFRAME.registerComponent("play-on-click",{
+    schema:{
+        isPlaying:{
+            type:"boolean",
+            default:false,
+        }
+    },
+    init: function(){
+        this.videoEL = this.el.getAttribute("material").src 
+        this.onClick = this.onClick.bind(this)
+    },
+    play: function(){
+        window.addEventListener("click",this.onClick)
+    },
+    onClick: function(ent){
+        if(!this.videoEL){
+            return
+        }
+        var isPlaying = this.el.getAttribute("play-on-click").isPlaying
+        this.el.object3D.visible = true
+        if(!isPlaying){
+            this.el.setAttribute("play-on-click",{isPlaying:true})
+            this.videoEl.play()
+        }else{
+            this.el.setAttribute("play-on-click",{isPlaying:false})
+            this.videoEl.pause()
+        }
+    }
+})
